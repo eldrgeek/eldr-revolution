838b6db622a0513ae7a7aebc8d2b9c9e1f34a7f2
Mike Wolf <mw@mike-wolf.com>
2019-07-26 20:10:46 -0400

    Show lyrics or read words

--- src/YouTube.js
+++ src/YouTube.js
@@ -1,13 +1,37 @@
 import React from "react";
 import ReactPlayer from "react-player";
 import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Iframe from "react-iframe";
+const useStyles = makeStyles(theme => ({
+  linkButton: {
+    backgroundColor: "transparent",
+    border: "none",
+    cursor: "pointer",
+    textDecoration: "underline",
+    fontSize: "inherit",
+    display: "inline",
+    margin: "0",
+    padding: "0"
+  }
 
+  // linkButton:hover,
+  // link-button:focus {
+  // text-decoration: none;
+  // }
+}));
 const url =
-  // "https://www.youtube.com/embed/o507bg_K6hs?rel=0;&autoplay=1;&mute=1";
-  "https://vimeo.com/291764638";
-const RevolutionVideo = () => {
+  "https://www.youtube.com/embed/o507bg_K6hs?rel=0;&autoplay=1;&mute=1";
+// "https://vimeo.com/291764638";
+const RevolutionVideo = props => {
+  const classes = useStyles();
   const [message, setMessage] = React.useState("no message");
+  const [playing, setPlaying] = React.useState(false);
+  const [showLyrics, setShowLyrics] = React.useState(false);
 
+  const startPlaying = () => {
+    setPlaying(true);
+  };
   return (
     <React.Fragment>
       {/* <Message /> */}
@@ -17,14 +41,44 @@ const RevolutionVideo = () => {
         </Typography>
       ) : (
         <React.Fragment>
-          {message != "playing" ? "Click to hear our anthem" : ""}
+          {message !== "playing" ? (
+            <Typography align="center" variant="h5" component="h1" gutterBottom>
+              <button className={classes.linkButton} onClick={startPlaying}>
+                Play our anthem
+              </button>{" "}
+              or{" "}
+              <button
+                className={classes.linkButton}
+                onClick={() => setShowLyrics(true)}
+              >
+                read the words
+              </button>
+            </Typography>
+          ) : (
+            ""
+          )}
           <RevolutionPlayer
+            showLyrics={showLyrics}
             message={message}
+            playing={playing}
             onStart={() => setMessage("starting")}
             onEnded={() => setMessage("ended")}
             onPlay={() => setMessage("playing")}
-            onPaused={() => setMessage("paused")}
+            onPause={() => setMessage("paused")}
           />
+          {showLyrics ? (
+            <Iframe
+              url="https://www.noelpaulstookey.com/14-12.html"
+              width="650px"
+              height="1000px"
+              id="myId"
+              className="myClassname"
+              display="initial"
+              position="relative"
+            />
+          ) : (
+            ""
+          )}
         </React.Fragment>
       )}
     </React.Fragment>
@@ -35,6 +89,7 @@ const RevolutionPlayer = props => {
     <React.Fragment>
       <ReactPlayer
         url={url}
+        playing={props.playing}
         config={{
           youtube: {
             playerVars: {
