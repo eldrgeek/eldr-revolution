a1132a234f7878f8f1863577599f3bc73a0247d7
Mike Wolf <mw@mike-wolf.com>
2019-09-18 17:35:41 -0400

    Just logo and coming soon

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import React from "react";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import { makeStyles } from "@material-ui/core/styles";
+import YouTube from "./YouTube";
+
+const useStyles = makeStyles(theme => ({
+  
+  still: {
+    // position: "absolute",
+    width: "100%"
+  },
+  text: {
+    color: "white",
+    textAlign: "center"
+  }
+
+ 
+}));
 // import Link from "@material-ui/core/Link";
 // import ProTip from "./ProTip";
-import YouTube from "./YouTube";
 function MadeWithLove() {
   return (
     <Typography variant="body2" color="textSecondary" align="left">
@@ -18,6 +33,7 @@ function MadeWithLove() {
 }
 
 export default function App() {
+   const classes = useStyles();
   return (
     <Container style={{ background: "black" }} maxWidth="sm">
       <Box style={{ background: "black" }} my={1}>
@@ -27,8 +43,20 @@ export default function App() {
         <Typography align="left" variant="h4" component="h1" gutterBottom>
           The Revolution is coming near you.
         </Typography>
+        <div>
+        <img
+              style={{ visibility: true}}
+              id="still"
+              className={classes.still}
+              alt="revolution 1x1"
+              src="revolution still.jpg"
+            />
+        </div>
+        <Typography  className={classes.text} align="left" variant="h6" component="h1" gutterBottom>
+          coming soon
+        </Typography>
         {/* <ProTip /> */}
-        <YouTube />
+        {/* <YouTube /> */}
 
         <MadeWithLove />
       </Box>
